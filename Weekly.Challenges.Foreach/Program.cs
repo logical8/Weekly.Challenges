85296875036f2f611485a93a84e05c67daa6df8a
alekkireev <alekkireev@gmail.com>
2023-06-21 19:22:15 +0400

    1. Foreach challenge

---
+++ Weekly.Challenges.Foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿var persons = new List<Person>
+{
+    new("Van", "Diesel"),
+    new("Obivan", "Keenoby"),
+    new("Jan Clodt", "Vandamm")
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine($"{person.Name} {person.LastName}");
+}
+
+class Person
+{
+    public Person(string name, string lastName)
+    {
+        Name = name;
+        LastName = lastName;
+    }
+
+    public string Name { get; }
+    public string LastName { get; }
+}
